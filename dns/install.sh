abfd0f65f727ba0a60807b65fbc14f1382e05bb9
Clement Thuault <thuaultc@gmail.com>
2019-02-25 00:12:22 +0100

    Remove useless install script

--- dns/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-TERRAFORM_VERSION="0.11.11"
-DOCKER_TERRAFORM="docker run --rm -v $(pwd):/app/ -w /app/ -it hashicorp/terraform:$TERRAFORM_VERSION"
-
-$DOCKER_TERRAFORM init
-$DOCKER_TERRAFORM apply
