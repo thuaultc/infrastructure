c067e39ed00b3d50e02c9ae16baa89e5a5c911ee
Clement Thuault <thuaultc@gmail.com>
2019-02-24 22:19:30 +0100

    Add RPN handling and RKE cluster config generation

---
+++ scripts/kubernetes-cluster-generate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_NAME="$(basename $0)"
+
+display_usage() {
+  echo "usage: $SCRIPT_NAME <cluster-name> <online-token>"
+  exit 1
+}
+
+if [ $# -lt 2 ]; then
+  display_usage
+fi
+
+SCRIPT_PATH="$(cd "$(dirname "$0")"; pwd -P)"
+
+DIRECTORY_PATH="$SCRIPT_PATH/../kubernetes/cluster"
+DOCKER_TAG="infrastructure/kubernetes-cluster"
+
+KUBERNETES_OUTPUT_PATH="$DIRECTORY_PATH/../output"
+
+# Arguments
+CLUSTER_NAME=$1
+ONLINE_TOKEN=$2
+
+# Main
+cd $DIRECTORY_PATH
+echo "Building docker image..."
+docker build --build-arg UID=`id -u` --build-arg GID=`id -g` -t $DOCKER_TAG . > /dev/null 2> /dev/null
+echo "Launching image..."
+docker run --rm -v $KUBERNETES_OUTPUT_PATH:/output:rw -e ONLINE_TOKEN=$ONLINE_TOKEN -it $DOCKER_TAG generate $CLUSTER_NAME
---
+++ scripts/kubernetes-cluster-remove.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_NAME="$(basename $0)"
+
+display_usage() {
+  echo "usage: $SCRIPT_NAME <cluster-name> <server-id> <online-token>"
+  exit 1
+}
+
+if [ $# -lt 3 ]; then
+  display_usage
+fi
+
+SCRIPT_PATH="$(cd "$(dirname "$0")"; pwd -P)"
+
+DIRECTORY_PATH="$SCRIPT_PATH/../kubernetes/cluster"
+DOCKER_TAG="infrastructure/kubernetes-cluster"
+
+KUBERNETES_OUTPUT_PATH="$DIRECTORY_PATH/../output"
+
+# Arguments
+CLUSTER_NAME=$1
+SERVER_ID=$2
+ONLINE_TOKEN=$3
+
+# Main
+cd $DIRECTORY_PATH
+echo "Building docker image..."
+docker build --build-arg UID=`id -u` --build-arg GID=`id -g` -t $DOCKER_TAG . > /dev/null 2> /dev/null
+echo "Launching image..."
+docker run --rm -v $KUBERNETES_OUTPUT_PATH:/output:rw -e ONLINE_TOKEN=$ONLINE_TOKEN -it $DOCKER_TAG remove $CLUSTER_NAME $SERVER_ID
