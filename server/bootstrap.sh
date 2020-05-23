60a2c967039ad97dc81037d6bd617bad3b85f80f
Clement Thuault <thuaultc@gmail.com>
2020-05-23 14:15:12 +0200

    Add github.com keyscan

--- server/bootstrap.sh
+++ server/bootstrap.sh
@@ -2,6 +2,7 @@
 
 set -e
 
-git clone -o StrictHostKeyChecking=no git@github.com:thuaultc/infrastructure.git
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+git clone git@github.com:thuaultc/infrastructure.git
 
 infrastructure/server/install.sh > install.log 2>&1
