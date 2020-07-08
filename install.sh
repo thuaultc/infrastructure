c591f4efad331a9684a2175382a368a004381d9c
Clement Thuault <thuaultc@gmail.com>
2020-07-08 21:23:08 +0200

    Rename kube user, add ssh key, add repository clone

--- install.sh
+++ install.sh
@@ -9,11 +9,3 @@ echo "[server]" >> /etc/ansible/hosts
 echo "$HOSTNAME ansible_host=127.0.0.1" >> /etc/ansible/hosts
 
 ansible-playbook /root/infrastructure/server/install.yml
-
-su common
-cd /home/common
-
-git clone git://github.com/thuaultc/infrastructure.git
-git clone git://github.com/thuaultc/kubernetes.git
-
-ansible-playbook infrastructure/server/kubernetes.yml
